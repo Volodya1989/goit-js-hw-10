57b472f46063a9be04f59fc8d80f1d8b7d98cbab
Volodya1989 <Vpetrytsia@gmail.com>
2023-05-05 07:26:48 +0000

    Deploying to gh-pages from @ Volodya1989/goit-js-hw-10@ff5ce537e53f68ffa0be91085bf6e22827b2f784 🚀

--- index.1e63a43d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input#search-box").addEventListener("input",(e=>{console.log(e)}));
-//# sourceMappingURL=index.1e63a43d.js.map
--- index.e08481be.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input#search-box").addEventListener("input",(function(e){console.log(e)}));
-//# sourceMappingURL=index.e08481be.js.map
